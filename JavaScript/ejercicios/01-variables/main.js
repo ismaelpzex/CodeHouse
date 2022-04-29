ba6c97b17e8364490036d44acfdb30bb4ca44e35
ismaelpzex <ismaelpzex@gmail.com>
2022-04-29 13:18:54 +0200

    add exercise variables

---
+++ JavaScript/ejercicios/01-variables/main.js
@@ -0,0 +1,22 @@
+//================ 1 ====================
+
+const horas = 24
+const minutos = horas * 60
+const segundos = minutos * 60
+var segundosDia = horas * minutos * segundos
+console.log(segundosDia)
+
+//================ 2 ====================
+
+let euros = 7
+let dolares = 2
+euros*=dolares
+console.log(euros)
+
+//================ 3 ====================
+
+const IVA = 1.21
+let precio = 135
+precio*=IVA
+const mensaje = `El precio del articulo es ${precio} €`
+console.log(mensaje)
