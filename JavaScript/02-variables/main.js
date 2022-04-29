d1ae042e1873f06ce41deec269519982b8dfc77b
ismaelpzex <ismaelpzex@gmail.com>
2022-04-29 10:36:46 +0200

    variables

---
+++ JavaScript/02-variables/main.js
@@ -0,0 +1,21 @@
+// ! Variables
+
+//Forma tradicional de declarar variables -> ya no se recomienda su uso
+var edad = 35
+
+// Forma actual de hacerlo con let y const
+
+// Con let, creamos una variable, que nos permita cambiarle el varlor más adelante
+let nombre = 'Ismael'
+
+// Con const, creamos una variable, que no nos permite cambiarle el valor más adelante
+const altura = 1.82
+
+// Podemos crear una variable sin asignarle un valor.
+let nombreNovia
+
+
+
+const nombreNovia2 = 'lorena'
+
+nombreNovia2 = 'hola'
