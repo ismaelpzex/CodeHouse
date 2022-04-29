1d46062d913d5ba8cc2fc1a0fdd3d8e862b8fdae
ismaelpzex <ismaelpzex@gmail.com>
2022-04-29 10:37:09 +0200

    debuggin

---
+++ JavaScript/03-debuggin/main.js
@@ -0,0 +1,20 @@
+//DEBUGGIN
+
+// console.log() -> imprime en consola //? EL QUE MÁS SE UTILIZA
+console.log('hola')
+
+//console puede imprimir varias cosas.
+console.error('imprime un error')
+console.warn('imprime un warning')
+console.time('crono')
+console.log('Contamos el tiempo que tarda la sentencia')
+console.timeEnd('crono')
+
+// alert() -> imprime en el NAVEGADOR una Ventana de alerta //! EL CODIGO SE PARA HASTA QUE ACEPTES
+alert('Esto es un coñazo')
+
+// document.write() -> escribe en el documento html //! HAY QUE PONERLO EN EL HTML ¡CACA!
+document.write('hola')
+
+// confirm() -> es un alert que da opcion de aceptar o confirmar
+confirm('estás seguro?')
