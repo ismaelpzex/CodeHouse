465192d873ad60721ea8d8fe15339c2408e8ac10
ismaelpzex <ismaelpzex@gmail.com>
2022-05-04 11:13:02 +0200

    functions

---
+++ JavaScript/10-funciones/main.js
@@ -0,0 +1,79 @@
+//! MUY IMPORTANTE
+//? Diferencia entre guardar en una variable por "valor" o por "referencia"
+// Valor
+let nombre = 'Ismael'
+nombre += ' de la Poza'
+// Referencia
+const frutas = ['manzana', 'platano']
+const otrasFrutas = frutas
+otrasFrutas[2] = 'uvas'
+
+console.log(frutas, otrasFrutas) // se guarda en los 2 arrays, tienen ese comportamiento
+
+// para duplicar un array se utiliza un spread operator
+
+const misFrutas = [...frutas]
+misFrutas[3] = 'limon'
+console.log(frutas) // ORIGINAL
+console.log(misFrutas) // DUPLICADO + LIMON
+
+//! FUNCIONES
+//? Las funciones son bloques de código reutilizables
+
+//crear la función
+function saludo() {
+    console.log('Hola')
+}
+
+//invocar la función
+saludo()
+
+//particularmente si usamos la palabra funcion, podemos invocarla antes de crearla
+
+// da igual donde crees la función, puedes llamarla desde cualquier sitio
+
+despedida()
+
+function despedida() {
+    console.log('adios')
+}
+
+// para añadirle dinamismo, podemos pasarle parametros
+
+function saludoPersonalizado(nombre) {
+    console.log(`Hola, ${nombre}`)
+}
+
+saludoPersonalizado('Ismael')
+
+// si no me envian el parámetro, podemos dar un valor por detecto, o resolviendolo dentro de la función
+
+//? VALOR POR DEFECTO
+
+function buenosdias(nombre = 'amijito') {
+    console.log(`buenos días ${nombre}`)
+}
+
+buenosdias()
+buenosdias('Ismael')
+
+//? RESOLVER PROBLEMA DENTRO DE LA FUNCION
+
+function buenasTardes(nombre) {
+    if (!nombre) {
+        console.log('buenas tardes amijo')
+    } else {
+        console.log(`buenas tardes ${nombre} `)
+    }
+}
+
+buenasTardes()
+buenasTardes('Ismael')
+
+// Pasar mas de un parámetro
+
+function suma(a, b) {
+    console.log(a + b)
+}
+
+suma(5,5)
