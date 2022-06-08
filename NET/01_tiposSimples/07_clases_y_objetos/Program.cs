dbbae7a0d996b62a18e6aef913b9cb5673bcadbf
ismaelpzex <ismaelpzex@gmail.com>
2022-06-08 11:37:02 +0200

    add class example

--- NET/01_tiposSimples/07_clases_y_objetos/Program.cs
+++ NET/01_tiposSimples/07_clases_y_objetos/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using viviendas.clases;
+using cocheras.clases;
 
+Vivienda nuevaVivienda = new Vivienda(90, "Calle test, Jaén");
+
+Console.WriteLine($"La dirección de la vivienda es: {nuevaVivienda.Address}");
+Console.WriteLine($"Los metros de la vivienda son: {nuevaVivienda.Meters}");
+
+if (nuevaVivienda.isVpo())
+    Console.WriteLine("La vivienda es VPO");
+else
+    Console.WriteLine("La vivienda no es VPO");
+
+nuevaVivienda.Reform(90);
+
+if (nuevaVivienda.isVpo())
+    Console.WriteLine("La vivienda es VPO");
+else
+    Console.WriteLine("La vivienda no es VPO");
+
+cocheras
---
+++ NET/01_tiposSimples/07_clases_y_objetos/clases/vivienda.cs
@@ -0,0 +1,32 @@
+﻿namespace _07_clases_y_objetos.clases
+{
+    public class Vivienda
+	{
+		public int Meters { get; set; }
+		public string Address { get; set; }
+		public bool Pool { get; set; }
+		public string Onwer { get; set; }
+
+		public Vivienda(int meters, string address)
+		{
+			Meters = meters;
+			Address = address;
+		}
+
+		public bool isVpo()
+        {
+			return Meters <= 90;
+        }
+
+		public void Reform(int meters)
+        {
+			Meters += meters;
+        }
+
+		private bool HasOnwer()
+        {
+			return !string.IsNullOrEmpty(Onwer);
+        }
+	}
+}
+
