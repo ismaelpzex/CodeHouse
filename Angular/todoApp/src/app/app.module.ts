03a39d5aeece79311cfddd46d88d692e21371376
ismaelpzex <ismaelpzex@gmail.com>
2022-07-04 12:17:35 +0200

    install bootstrap and generate components

--- Angular/todoApp/src/app/app.module.ts
+++ Angular/todoApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaTareasComponent } from './components/lista-tareas/lista-tareas.component';
+import { FromComponent } from './components/from/from.component';
+import { FiltrosComponent } from './components/filtros/filtros.component';
+import { TareasComponent } from './components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaTareasComponent,
+    FromComponent,
+    FiltrosComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule
