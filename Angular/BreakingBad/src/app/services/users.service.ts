6fdfde54b93131384eb27e597c4cffe70e71f43f
ismaelpzex <ismaelpzex@gmail.com>
2022-07-15 13:49:14 +0200

    add login usersService

--- Angular/BreakingBad/src/app/services/users.service.ts
+++ Angular/BreakingBad/src/app/services/users.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  baseUrl: string = 'https://register-codehouse-app.herokuapp.com/'
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  login(formValue: User): Promise<any> {
+    return lastValueFrom(this.httpClient.post<any>(this.baseUrl, formValue));
+  }
 }
