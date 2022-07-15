edb2bc992741faca0fbbd283990520334c837628
ismaelpzex <ismaelpzex@gmail.com>
2022-07-15 13:28:12 +0200

    add components

--- Angular/BreakingBad/src/app/app.module.ts
+++ Angular/BreakingBad/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+import { CharacterViewComponent } from './components/character-view/character-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    CharactersListComponent,
+    CharacterCardComponent,
+    CharacterViewComponent
   ],
   imports: [
     BrowserModule,
